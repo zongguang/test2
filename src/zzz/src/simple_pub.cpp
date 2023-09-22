579e27a211e80bbddbb2825d4226fc43b73e637f
zongguang <zliu9@mtu.edu>
2023-09-22 14:51:34 +0000

    k

--- src/zzz/src/simple_pub.cpp
+++ src/zzz/src/simple_pub.cpp
@@ -8,7 +8,7 @@
 #include "std_msgs/msg/string.hpp"
 
 using namespace std::chrono_literals;
-
+// basbdbsbds
 /* This example creates a subclass of Node and uses std::bind() to register a
 * member function as a callback from the timer. */
 
